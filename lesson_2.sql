4b804842acf17b258883202182fc56fac05ee7fd
aachaso6 <asheludykova@gmail.com>
2021-12-07 22:27:16 +0300

    add homework_2 and homework_3

---
+++ lesson_2.sql
@@ -0,0 +1,18 @@
+﻿SELECT EXTRACT(year FROM o.o_date), EXTRACT(month FROM o.o_date), SUM(price) FROM orders_2019 o
+GROUP BY EXTRACT(year FROM o.o_date), EXTRACT(month FROM o.o_date);
+
+WITH table_user AS (SELECT o.user_id, o.o_date,
+LEAD (o.o_date) OVER (PARTITION BY o.user_id ORDER BY o.o_date) AS next_date,
+ROW_NUMBER() OVER (PARTITION BY o.user_id ORDER BY o.o_date) AS num_order
+ FROM (SELECT DISTINCT  o.user_id, o.o_date FROM orders_2019 o WHERE o.price IS NOT NULL) o
+ WHERE o.o_date IS NOT NULL)
+
+
+SELECT t1.*, t1.next_date-t1.o_date AS diff_day FROM table_user t1
+INNER JOIN (SELECT user_id, COUNT(id_o) FROM orders_2019
+WHERE user_id IS NOT NULL AND id_o IS NOT NULL AND price IS NOT NULL 
+GROUP BY user_id
+HAVING COUNT(id_o)>1) t2 ON t2.user_id = t1.user_id
+WHERE t1.num_order=1 AND t1.next_date IS NOT null
+ORDER BY t1.user_id;
+
---
+++ lesson_3.sql
@@ -0,0 +1,33 @@
+﻿-- В качестве ДЗ делам прогноз ТО на 05.2017. В качестве метода прогноза - считаем сколько денег тратят группы клиентов в день:
+
+-- 1. Группа часто покупающих (3 и более покупок) и которые последний раз покупали не так давно. 
+-- Считаем сколько денег оформленного заказа приходится на 1 день. Умножаем на 30.
+
+'''SELECT SUM(o.price), COUNT(o.o_date), SUM(o.price)/COUNT(o.o_date) AS sum_o_of_day 
+FROM (SELECT * FROM orders_2019 o1 WHERE MONTH(o1.o_date) IN (4) AND year(o1.o_date) = 2017 
+      GROUP BY o1.user_id HAVING COUNT(o1.id_o)>2
+      ORDER BY o1.user_id) o;'''
+
+SELECT COUNT(DISTINCT o.user_id), COUNT(DISTINCT o.id_o), SUM(o.price)/COUNT(o.o_date) * 30 AS avg_m_s FROM orders_2019 o WHERE o.o_date < date('2017-05-01') AND 
+o.user_id IN (SELECT o.user_id FROM orders_2019 o WHERE o.o_date < DATE('2017-05-01')
+GROUP BY o.user_id
+HAVING COUNT(o.id_o)>2 AND TIMESTAMPDIFF(DAY, MAX(o.o_date), date('2017-05-01')) < 90);
+
+-- 2. Группа часто покупающих, но которые не покупали уже значительное время. 
+-- Так же можем сделать вывод, из такой группы за след месяц сколько купят и на какую сумму. (постараться продумать логику)
+
+SELECT COUNT(DISTINCT o.user_id), COUNT(DISTINCT o.id_o), SUM(o.price)/COUNT(DISTINCT o.user_id) AS avg_m_s FROM orders_2019 o WHERE o.o_date >= date('2017-05-01') AND o.o_date < DATE('2017-06-01')
+AND o.user_id IN (SELECT o.user_id FROM orders_2019 o WHERE o.o_date < DATE('2017-05-01')
+GROUP BY o.user_id
+HAVING COUNT(o.id_o)>2 AND TIMESTAMPDIFF(DAY, MAX(o.o_date), date('2017-05-01')) >= 90);
+
+-- 3. Отдельно разобрать пользователей с 1 и 2 покупками за все время, прогнозируем их.
+SELECT COUNT(DISTINCT o.user_id), COUNT(DISTINCT o.id_o), SUM(o.price)/COUNT(DISTINCT o.user_id) AS avg_m_s 
+FROM orders_2019 o WHERE o.o_date >= date('2017-05-01') AND o.o_date < DATE('2017-06-01')
+AND o.user_id IN (SELECT o.user_id FROM orders_2019 o WHERE o.o_date < DATE('2017-05-01')
+GROUP BY o.user_id
+HAVING COUNT(o.id_o)<=2);
+
+-- 4. В итоге у вас будет прогноз ТО и вы сможете его сравнить с фактом и оценить грубо разлет по данным
+SELECT t1.*
+FROM (SELECT SUM(o.price), COUNT(DISTINCT o.id_o) FROM orders_2019 o WHERE o.o_date = DATE('2017-05-01')) t1;
